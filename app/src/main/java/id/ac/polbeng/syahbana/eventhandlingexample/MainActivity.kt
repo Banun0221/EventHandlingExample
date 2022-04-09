ac507ba8ad15a928379fb24f892c697090c47340
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-04-09 12:05:08 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polbeng/syahbana/eventhandlingexample/MainActivity.kt
@@ -0,0 +1,23 @@
+package id.ac.polbeng.syahbana.eventhandlingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDisplayMessage.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(applicationContext, "Hello World",
+                    Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
